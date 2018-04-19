81c4e6a29caeb7061a736c7d4b6e296f65d8277b
Alejo García Galán <alejogarciagalan@gmail.com>
2018-04-19 18:19:52 +0200

    fallo de trampolin

--- STring.c
+++ STring.c
@@ -3,64 +3,43 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
+
 #define BLQ 50
+#define DELI " ,.-;:_΅+`*^\n\t"
+#define DIR1 "/home/pi/Documents/PracticasC/STring/palabras2"
+#define DIR2 "/home/pi/Documents/PracticasC/STring/palabrasSeparadas"
 
-int estaEn(char,char*);
-void impLisPal(char*,char**,int);
+int estaEn(char __car, char* __str);
+void guardarLista(char* __d, char** __l, int __t);
+int cargarLista(char* __dir, char ** __dest, char* __sep);
 
-int main(){
-	char* dir = "/home/pi/Documents/PracticasC/STring/palabras2";
-	char* dir2 = "/home/pi/Documents/PracticasC/STring/palabrasSeparadas";
-	char* buff = calloc(BLQ,sizeof(char));
-	char** db;
-	char del[]= " ,.-;:_΅+`*^\n\t";
+int main(void)
+{
+	char** db = NULL;
 	int i;
-	char c;
-	int cl = 0;
-	int cp = 0;
-	FILE * f = fopen(dir,"r");
-	while (!feof(f))
-	{	
-		if(1!=fread(&c,sizeof(char),1,f)){
-			if (feof(f)) perror("Encontrado EOF");
-			else if (ferror(f)) perror("Error del fichero");
-		}else if(estaEn(c,del)){
-			if(cl != 0){
-				db = realloc(db,sizeof(char*)*(cp+1));
-				db[cp] = malloc(sizeof(char)*(cl+1));
-				strncpy(db[cp],buff,cl+1);
-				cp++;
-				cl = 0;
-			}
-		} else{
-			buff[cl] = (char)c;
-			buff[cl+1] = '\0';
-			cl++;
-		}	
-	} 
-	fclose(f);
+	int cp = cargarLista(DIR1, db, DELI); 
 	printf("\nSe han leido: %d palabras.\n\n",cp);
-	for (i = 0; i < cp; i++){
-		printf("%d:\t%s\n",strlen(db[i]),db[i]);
-	}
+	for (i = 0; i < cp; i++) printf("%d:\t%s\n",strlen(db[i]),db[i]);
 	printf("\n\n");
-	impLisPal(dir2,db,cp);
+	guardarLista(DIR2,db,cp);
 	printf("Escritura completada.\n\n");
 	return 0;
 }
 
-int estaEn(char car,char* str){
+int estaEn(char __car, char* __str)
+{
 	int r = 0;
-	char *aux = str;
-	while((r == 0)&&(*aux != '\0')){
-		if(car == *aux) r = 1;
-		aux++;
+	char *__aux = __str;
+	while((r == 0)&&(*__aux != '\0')){
+		if(__car == *__aux) r = 1;
+		__aux++;
 	}
 	return r;
 }
 
-void impLisPal(char* __d, char** __l, int __t){
-	FILE* fout = fopen(__d,"w+");
+void guardarLista(char* __d, char** __l, int __t)
+{
+	FILE * fout = fopen(__d,"w+");
 	int i;
 	char cr = '\n';
 	char *ptr;
@@ -76,4 +55,34 @@ void impLisPal(char* __d, char** __l, int __t){
 		}
 	}
 	fclose(fout);
+}
+
+int cargarLista(char* __dir, char ** __dest, char* __sep)
+{	
+	FILE * fin = fopen(__dir,"r");
+	char* buff = calloc(BLQ,sizeof(char));
+	char aux;
+	int clet = 0;
+	int cpal = 0;
+	while (!feof(fin))
+	{	
+		if(1!=fread(&aux,sizeof(char),1,fin)){
+			if (feof(fin)) perror("Encontrado EOF");
+			else if (ferror(fin)) perror("Error del fichero");
+		}else if(estaEn(aux,__sep)){
+			if(clet != 0){
+				__dest = realloc(__dest,sizeof(char*)*(cpal+1));
+				__dest[cpal] = malloc(sizeof(char)*(clet+1));
+				strncpy(__dest[cpal],buff,clet+1);
+				cpal++;
+				clet = 0;
+			}
+		} else{
+			buff[clet] = (char)aux;
+			clet++;
+			buff[clet] = '\0';
+		}	
+	} 
+	fclose(fin);
+	return cpal;
 }
